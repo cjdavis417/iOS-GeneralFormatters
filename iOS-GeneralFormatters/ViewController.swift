df3ce368f6e1ccca3760a26dca6f7872bdea3d2c
Chris Davis <christopherjdavis417@gmail.com>
2016-12-31 13:38:47 -0500

    Initial Commit

---
+++ iOS-GeneralFormatters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-GeneralFormatters
+//
+//  Created by Christopher Davis on 12/31/16.
+//  Copyright © 2016 Monkey Face LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
