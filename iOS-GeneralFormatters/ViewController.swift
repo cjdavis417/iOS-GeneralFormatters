a7d8e09fb21564494bd142acf69542f6f50d77c6
Christopher Davis <christopherjdavis417@gmail.com>
2018-02-04 15:06:03 -0500

    added examples

--- iOS-GeneralFormatters/ViewController.swift
+++ iOS-GeneralFormatters/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var todayDate: UILabel!
+    @IBOutlet weak var dateFull: UILabel!
+    @IBOutlet weak var dateLong: UILabel!
+    @IBOutlet weak var dateMedium: UILabel!
+    @IBOutlet weak var dateShort: UILabel!
+    
+    @IBOutlet weak var timeFull: UILabel!
+    @IBOutlet weak var timeLong: UILabel!
+    @IBOutlet weak var timeMedium: UILabel!
+    @IBOutlet weak var timeShort: UILabel!
+    
+    
+    let today = Date()
+    let gf = Formatters()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        todayDate.text = String(describing: today)
+        dateFull.text = gf.formattedDate(today, style: "full")
+        dateLong.text = gf.formattedDate(today, style: "long")
+        dateMedium.text = gf.formattedDate(today, style: "medium")
+        dateShort.text = gf.formattedDate(today, style: "short")
+        
+        timeFull.text = gf.formattedDate(today, style: "full")
+        timeLong.text = gf.formattedDate(today, style: "long")
+        timeMedium.text = gf.formattedDate(today, style: "medium")
+        timeShort.text = gf.formattedDate(today, style: "short")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
